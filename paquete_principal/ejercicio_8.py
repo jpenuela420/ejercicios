5fa06b6827267c691a2771b10e7d9978ee6b1a60
jpenuela420 <penuelaherrerajuanjose@gmail.com>
2023-08-01 18:54:23 -0500

    Este ejercicio es para invertir una lista dada

---
+++ paquete_principal/ejercicio_8.py
@@ -0,0 +1,6 @@
+def main():
+    lista = [15,22,53,4,23,41]
+    ListaInvertida= list(reversed(lista))
+    print(f"La lista dada es : {lista}")
+    print(f"La lista invertida es : {ListaInvertida}")
+main()
