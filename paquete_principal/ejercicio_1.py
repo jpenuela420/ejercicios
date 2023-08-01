32c779ccb78ada342d7b4377e96d4bfebf14718a
Jpenuela420 <penuelaherrerajuanjose@gmail.com>
2023-08-01 11:20:07 -0500

    Ejercicio para preguntar y seguidamente imprimir el nombre y la edad del usuario.

---
+++ paquete_principal/ejercicio_1.py
@@ -0,0 +1,5 @@
+def informacion():
+    Name= input("ingrese el nombre del usuario:")
+    Age= int(input("ingrese la edad del usuario:"))
+    print(f"El nombre del usuario es: {Name}, La edad del usuario es: {Age} años")
+informacion()
